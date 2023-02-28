edda6d12ae09af8ffa9ff1afecb731bd1b6e5d93
AJ <ancheetah@users.noreply.github.com>
2023-02-28 14:37:51 -0500

    fetch posts with post reducer

--- src/App.js
+++ src/App.js
@@ -1,27 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import { Provider } from 'react-redux'
 
 import Posts from './components/Posts'
 import PostForm from './components/Postform';
+import store from './store'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <PostForm />
-      <hr />
-      <Posts />
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+        <PostForm />
+        <hr />
+        <Posts />
+      </div>
+    </Provider>
   );
 }
 
--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,19 +1,13 @@
 import React, { Component } from 'react'
-
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { fetchPosts } from '../actions/postActions';
 class Posts extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            posts: []
-        }
-    }
     componentWillMount() {
-        fetch('https://jsonplaceholder.typicode.com/posts')
-        .then(res => res.json())
-        .then(data => this.setState({ posts: data}))
+      this.props.fetchPosts();
     }
   render() {
-    const postItems = this.state.posts.map(post => (
+    const postItems = this.props.posts.map(post => (
         <div key={post.id}>
             <h3>{post.title}</h3>
             <p>{post.body}</p>
@@ -28,4 +22,13 @@ class Posts extends Component {
   }
 }
 
-export default Posts;
+Posts.propTypes = {
+  fetchPosts: PropTypes.func.isRequired,
+  posts: PropTypes.array.isRequired
+}
+
+const mapStateToProps = state => ({
+  posts: state.posts.items
+  // the posts key comes from combineReducers
+})
+export default connect(mapStateToProps, { fetchPosts })(Posts);
